31f9899c95b68a935c0e11b0f95abf0f174bdff5
Vladimir <cobalt1203@gmail.com>
2020-11-10 15:44:30 +0400

    update database

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -13,6 +15,8 @@ type Product struct {
 	Slug  string
 	Price float64
 }
+
+// Database interface
 type Database interface {
 	GetProducts(ctx context.Context, limit int, offset int) (result []Product, err error)
 	GetProductByID(ctx context.Context, id int) (result Product, err error)
@@ -37,9 +41,35 @@ func New() (*DB, error) {
 
 // GetProducts func
 func (d *DB) GetProducts(ctx context.Context, limit int, offset int) (result []Product, err error) {
-	q := "SELECT id, name, slug, price FROM Product LIMIT ;"
+	q := fmt.Sprintf("SELECT id, name, slug, price FROM Product LIMIT %d OFFSET %d;", limit, offset)
 	if err = d.conn.SelectContext(ctx, &result, q); err != nil {
 		return nil, err
 	}
 	return result, err
+}
+
+// GetProductByID func
+func (d *DB) GetProductByID(ctx context.Context, id int) (result Product, err error) {
+	if err := d.conn.Get(&result, "SELECT id, name, slug, price FROM Product WHERE id=$1", id); err != nil {
+		// проверка на sql ErrNoRows
+		if err == sql.ErrNoRows {
+			fmt.Errorf("No Rows Error")
+		} else {
+			return result, err
+		}
+
+	}
+	return result, nil
+}
+
+// CreateProduct func
+func (d *DB) CreateProduct(ctx context.Context, pr Product) (int, error) {
+	q := "INSERT INTO Product (name, slug, price) VALUES ($1, $2, $3) returning id;"
+	var id int
+	err := d.conn.GetContext(ctx, &id, q, pr.Name, pr.Slug, pr.Price)
+
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -14,16 +14,19 @@ type Product struct {
 	Price float64
 }
 
+//Database interface
 type Database interface {
 	GetProducts(ctx context.Context, limit int, offset int) (result []database.Product, err error)
 	GetProductByID(ctx context.Context, id int) (result database.Product, err error)
 	CreateProduct(ctx context.Context, pr database.Product) (id int, err error)
 }
 
+//Service struct
 type Service struct {
 	db Database
 }
 
+//New func
 func New(db Database) (*Service, error) {
 
 	return &Service{
@@ -31,6 +34,36 @@ func New(db Database) (*Service, error) {
 	}, nil
 }
 
+//GetProducts func
 func (s *Service) GetProducts(ctx context.Context, limit, offset int) (result []Product, err error) {
-	s.db.GetProducts(ctx, limit, offset)
+	res, err := s.db.GetProducts(ctx, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	result = make([]Product, len(res))
+	for i, val := range res {
+		result[i] = Product{
+			ID:    val.ID,
+			Name:  val.Name,
+			Slug:  val.Slug,
+			Price: val.Price,
+		}
+	}
+	return result, nil
+}
+
+// GetProductByID func
+func (s *Service) GetProductByID(ctx context.Context, id int) (result Product, err error) {
+	dbProduct, err := s.db.GetProductByID(ctx, id)
+	result.ID, result.Name, result.Price, result.Slug = dbProduct.ID, dbProduct.Name, dbProduct.Price, dbProduct.Slug
+	return result, nil
+}
+
+// CreateProduct func
+func (s *Service) CreateProduct(ctx context.Context, pr database.Product) (id int, err error) {
+	id, err = s.db.CreateProduct(ctx, pr)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
 }
